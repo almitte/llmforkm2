b5188fcc64aa5cc4d644c52eab921e98353e50f5
Max Schneider <max1912sch@gmail.com>
2024-04-29 09:42:37 +0200

    login etc

--- rag.py
+++ rag.py
@@ -5,31 +5,45 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import ChatPromptTemplate 
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langsmith import Client
+from langchain.callbacks import LangChainTracer
 
+# load .env Variablen 
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+# tracing mit Langsmith from Langchain
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "RAG-project"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+client = Client()
 
-def initialize_everything(name):
-    load_dotenv()
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# konvertiert AI Message zu einem String
+parser = StrOutputParser() 
+
+# template prompt
+# template = """
+# Beantworte die Frage basierend auf dem gegebenen Kontext: {context}
+    
+    
+# Wenn das Beantworten der Frage nicht möglich ist durch den gegebenen Kontekt, antworte "Ich weiß es nicht". 
+# """
+# placeholder weil noch kein Kontext vorhanden
+template="Beantworte die Frage"
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", template),
+    MessagesPlaceholder(variable_name="history"),
+    ("human", "{question}"),
+])
+
+def initialize_chain(name):
+    
     model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model=name)
-    parser = StrOutputParser() 
-
-    # template = """
-    # Beantworte die Frage basierend auf dem gegebenen Kontext: {context}
-    
-    
-    # Wenn das Beantworten der Frage nicht möglich ist durch den gegebenen Kontekt, antworte "Ich weiß es nicht". 
-    # """
-    template="Beantworte die Frage"
-
-    prompt = ChatPromptTemplate.from_messages([
-            ("system", template),
-            MessagesPlaceholder(variable_name="history"),
-            ("human", "{question}"),
-    ])
 
     chain = prompt | model | parser 
-    
     
     return chain 
 
@@ -39,17 +53,32 @@ def generate_response(message, history_list, chain):
         # verschiedene Wrapper für verschiedene roles
         m = HumanMessage(content=mes["content"]) if mes["role"] == "user" else AIMessage(content=mes["content"])
         history.append(m)
-
-    response = chain.invoke({
+        
+    # .stream statt .invoke um Antwort zu streamen
+    return chain.stream({
         "history": history, 
         "question": message,
+        # noch kein Kontext
         "context": ""
-        })  
-    
-    return response
+        })
 
-message = "Wie heiße ich?"
-history_list=[{"role":"user", "content": "Ich heiße Max"}, {"role": "assistant", "content": "Schön dich kennen zu lernen"}]
 
-chain = initialize_everything("gpt-4")
-print(generate_response(message, history_list, chain))
+def send_feedback(run_id,score):
+    key =f"feedback_{run_id}"
+    client.create_feedback(
+        run_id,
+        key=key,
+        score=score,
+        )
+        
+
+# Einfluss des Chatverlaufs verringer bei der Abfrage der Vektor-Datenbank (REWRITING)
+# retriever = vectorStore.as_retriever(search_kwargs={"k":3})
+# retriever_prompt = ChatPromptTemplate.from_messages([MessagesPlaceholder(variable_name = "history")], 
+#                                                     ("human", {input}),
+#                                                     ("human", "Mit der gegebenen Konversation, generiere eine Suchabfrage zum Nachschlagen, um Informationen zu erhalten, die für die Konversation relevant sind."))
+
+# history_aware_retriever = create_history_aware_retriever(
+#     llm=model, retriever=retriever, prompt=retriever_prompt
+# )
+
--- ui.py
+++ ui.py
@@ -1,77 +1,128 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 import rag
 import data
-
+import yaml
+from yaml.loader import SafeLoader
 # streamlit run interface.py
 
-st.title("Knowledge Management Chat")
-
-col1, col2 = st.columns(2)
-language = col1.selectbox("Sprache:", ["deutsch", "englisch"])
-version = col2.selectbox("Version:", ["gpt-3.5-turbo", "gpt-4","gpt-4-turbo"])
-
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = [] 
-### Modelwechsel funktioniert nichts    
-if version == "gpt-4-turbo":
-    chain = rag.initialize_everything(version)
-
-if version == "gpt-4":
-    chain = rag.initialize_everything(version)
-
-if version == "gpt-3.5-turbo":
-    # model, parser und chain inistialisieren
-    chain = rag.initialize_everything(version)
-
-
-clear = col1.button("Clear")
-upgrade = col2.button("Update")
-
-if upgrade: 
-    with st.status("Es dauert noch einen kleinen Moment"):    
-    # function die lange dauert 
-    # update die Knowledge Base 
-        data.update_data()
-        st.write("Daten wurden erfolgreich geupdated")
-
-if clear: 
-    st.session_state.messages = []  
 
 
 
-with st.chat_message("assistant"):
-        st.write("Hallo, wie kann ich behilflich sein?")     
+with st.sidebar:
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])       
-        
-# React to user input
-prompt = st.chat_input("Stellen Sie eine Frage")
-if prompt :
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    )
+    authenticator.login()
+
+    if st.session_state["authentication_status"]:
+        authenticator.logout()
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect')
+    elif st.session_state["authentication_status"] is None:
+        st.warning('Please enter your username and password')
+    language = st.selectbox("Sprache:", ["deutsch", "englisch"])
+    version = st.selectbox("Version:", ["gpt-3.5-turbo", "gpt-4","gpt-4-turbo"])
+
+# Chatbot nur anzeigen wenn eingeloggt
+if st.session_state["authentication_status"]:
+    st.title("Knowledge Management Chat")
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = [] 
+
+    # Modellwechsel 
+    if version:
+        chain = rag.initialize_chain(version)
+
+    col1, col2 = st.columns(2)
+    clear = col1.button("Clear")
+    upgrade = col2.button("Update")
+
+    if upgrade: 
+        with st.status("Es dauert noch einen kleinen Moment"):    
+        # function die lange dauert 
+        # update die Knowledge Base 
+            data.update_data()
+            st.write("Daten wurden erfolgreich geupdated")
+
+    if clear: 
+        st.session_state.messages = []  
+
+
+
+    # with st.chat_message("assistant"):
+    #        st.write("Hallo, wie kann ich behilflich sein?")     
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])       
+
+    # session variable for prompt because it would go to false if the feedback buttons are clicked and then they wouldnt be reachible
+    if "prompt_" not in st.session_state:
+        st.session_state.prompt_ =  False 
     
-    # Display assistant response in chat message container
-    if st.session_state.messages[-1]["role"]!="assistant":
-       
-        with st.spinner("Ich muss nachdenken..."):
-            # nur die letzten 6 Nachrichten beachten
-            history = st.session_state.messages[-10:]         
+    # function that changes session_state             
+    def callback():
+        st.session_state.prompt_ =  True 
+
+    # speichern ther id der aktuellen Antwort    
+    if "run_id" not in st.session_state:
+        st.session_state.run_id = ""
+    
+    if "buttons" not in st.session_state:
+        st.session_state.buttons = [False, False]    
+
+    # React to user input
+    prompt = st.chat_input("Stellen Sie eine Frage", on_submit=callback)
+    #st.markdown(prompt)
+    #st.markdown(st.session_state.prompt_)
+    if st.session_state.prompt_:
+        if prompt != None:
+            st.session_state.buttons = [False, False] # reset buttons
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display assistant response in chat message container
+        if st.session_state.messages[-1]["role"]!="assistant":
+            # nur die letzten 10 Nachrichten beachten
+        # history = st.session_state.messages[-10:]         
             # Antwort auf Basis des Verlaufs und des Prompts generieren
-            response = rag.generate_response(prompt, history, chain)
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response}) 
-            
             with st.chat_message("assistant"):
-                st.markdown(response)
-        
+                # Tracing der run_id
+                from langchain.callbacks import collect_runs
+                with collect_runs() as cb:
+                    # Data wird gestreamt
+                    response = st.write_stream(rag.generate_response(prompt, st.session_state.messages[-10:]   , chain))
+                    # run-id zum feedback senden
+                    st.session_state.run_id=cb.traced_runs[0].id
+            # Add assistant response to chat history  
+            st.session_state.messages.append({"role": "assistant", "content": response})            
+        # feedback buttons
+        col1, col2 = st.columns(2)
+        if sum(st.session_state.buttons)==0:
+            if col1.button("👍"): 
+                rag.send_feedback(st.session_state.run_id,1)
+                st.session_state.buttons[0] = True
+                st.rerun()
+            if col2.button("👎"):
+                rag.send_feedback(st.session_state.run_id,0) 
+                st.session_state.buttons[1] = True 
+                st.rerun()
+            
+   
+
+
+            
 
 
 
